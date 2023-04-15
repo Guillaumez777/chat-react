138d978e08a45537b3b4da402281bf731ab2c586
Guillaume Montagné <g.montagne777@gmail.com>
2023-04-15 07:53:19 +0200

    ajout input

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Chat() {
   return (
     <div>
-        
+        <input type="text" placeholder='enter some text'/>
+        <button>Envoyer</button>
     </div>
   )
 }
